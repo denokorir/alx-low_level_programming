24779abbf3facbe2f78cc99c03b9db0e8fdaacae
denokorir <deniskorir@gmail.com>
2023-02-16 18:21:23 -0800

    print bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print size 
- * Return: Always 0 (success)
+ * main - print size of various types on the computer it is compiled and run on 
+ * Return: Always 0 (Success) 
  */
 
 int main(void)
