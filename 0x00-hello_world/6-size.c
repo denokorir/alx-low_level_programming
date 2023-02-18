ff0dce3e853c8a9e51042d83e74fc4fd70cc8aae
denokorir <deniskorir@gmail.com>
2023-02-17 22:50:32 -0800

    size of bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * print size of bytes in characters
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("size of char: %zu bytes\n", sizeof(char));
-	printf("size of int: %zu bytes\n", sizeof(int));
-	printf("size of long int: %zu bytes\n", sizeof(long int));
-	printf("size of long long int: %zu bytes\n", sizeof(long long int));
-	printf("size of float: %zu bytes\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
