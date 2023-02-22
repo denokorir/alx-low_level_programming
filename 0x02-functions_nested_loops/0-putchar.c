a8fe4b909a4b9eb0e7ca0eea742f9cedd563f59e
denokorir <deniskorir@gmail.com>
2023-02-21 16:54:16 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	char letter[] = "_putchar"
+	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch <= 8; ch++)
-		_putchar(letter[ch]);
+	{
+		_putchar(str[ch]);
+	}
 	_putchar('\n');
 	return (0);
 }
