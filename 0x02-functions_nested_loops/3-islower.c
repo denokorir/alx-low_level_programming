f415998cdc0d66ed543fa9531f0806d335506a09
denokorir <deniskorir@gmail.com>
2023-02-21 17:28:11 -0800

    check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checking for lowercase
+ * @c: interger value
+ *Return: lower if true, upper if false
+ */
+int _islower(int c)
+{
+	int x = 'a';
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (c == x)
+		{
+			return (lower);
+		}
+	}
+	return (upper);
+}
