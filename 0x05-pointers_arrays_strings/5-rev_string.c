c3597fe8d00dc45ee815e382232c3ed63cb5c12e
denokorir <deniskorir@gmail.com>
2023-02-27 17:46:47 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: variable
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int x, y;
+	char ch;
+
+	for (x = 0; s[x] != '\0'; ++x)
+
+	for (y = 0; y < x / 2; ++y)
+	{
+		ch = s[y];
+		s[y] = s[x - 1 - y];
+		s[x -1 - y] = ch;
+	}
+}
