ee2dcf2157a8c021ef45de08e26e848ef34227bc
denokorir <deniskorir@gmail.com>
2023-02-27 17:13:54 -0800

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: variable
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		++i;
+	while (--i)
+		_putchar(s[i]);
+	_putchar('\n');
+}
