12e0a1674892b07464b349c11a1e4cfed022f33c
denokorir <deniskorir@gmail.com>
2023-03-07 00:03:50 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,10 @@ void print_chessboard(char (*a)[8])
 {
 	int x, y;
 
-	for (int x = 0; x < 8; x++)
+	for (x = 0; x < 8; x++)
 	{
-		for (int y = 0; y < 8; y++)
-		{
+		for (y = 0; y < 8; y++)
 			_putchar(a[x][y]);
-		}
 		_putchar('\n');
 	}
 }
