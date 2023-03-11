b6e8078a3b853442f4d41932b0203e3e8aa1e47c
denokorir <deniskorir@gmail.com>
2023-03-11 00:01:58 -0800

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints a name
+ * @argv: array string
+ * @argc: line count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
