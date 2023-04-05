90642e31d4599fd60d899fb0ba3cba7e0f93913c
denokorir <deniskorir@gmail.com>
2023-04-04 18:11:03 -0800

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,7 +3,7 @@
  * free_listint - free a listint_t list
  * @head: pointer to the head of the list
  */
-void free_listint_t(listint_t *head)
+void free_listint(listint_t *head)
 {
 	listint_t *hold;
 
